0cdfa829a56b3b0d909b84aba965bc2ddc590683
fransi-kurti <fransi.kurti@gmail.com>
2023-02-21 18:22:42 +0100

    darkMode_lightMode css and javascript update

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const inputEl = document.getElementById("input");
+const inputEl = document.getElementById("dark-mode");
+const body = document.querySelector("body");
+
+
+changeBodyColor();
+
+function changeBodyColor(){
+    if(inputEl.checked){
+        body.style.background = "black";
+    } else{
+        body.style.background = "azure";
+    }
+}
+
+inputEl.addEventListener("input", ()=>{
+    changeBodyColor();
+})
